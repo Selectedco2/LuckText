0bfef1c2d73f68198c50e0222765d8dd5a708aec
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2023-08-31 10:26:52 +0200

    Första programmerings uppgift

---
+++ LuckText/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Vad heter du?");
+
+string Namn = Console.ReadLine();
+
+Console.WriteLine("Vart bor du?");
+
+string adress = Console.ReadLine();
+
+Console.WriteLine("Nämn en dryck");
+
+string dryck = Console.ReadLine();
+
+Console.WriteLine(Namn + " var påväg hem till " + adress + " när han märkte att han hade glömt att köpa " + dryck + ". Vad skulle nu hans mamma säga? " + Namn + " vågade knappt ens föreställa sig vad hans pappa skulle GÖRA.");
+
+
+
+
+Console.ReadLine();
